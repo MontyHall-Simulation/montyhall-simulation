5e341ff6c134e67ff8be83d8cdc4247ddddbc62d
seakjh <skykjh0513@gmail.com>
2023-05-02 17:30:34 +0900

    fix: Fix for statement indentation

--- montyhall.py
+++ montyhall.py
@@ -11,9 +11,9 @@ for _ in range(trial):
     player_choice = random.randint(0,2)
 
     empty_door = []
-     for i in range(3):
-        if i!=player_choice and i != car:
-            empty_door.append(door[i])
+    for i in range(3):
+       if i!=player_choice and i != car:
+           empty_door.append(door[i])
 
     com = random.sample(empty_door, 1)[0]
 
